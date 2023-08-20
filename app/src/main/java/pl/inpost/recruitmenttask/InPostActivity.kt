1f6153a0bd3b445991fc9bdda7fc531e9de0a840
Gabriel Goto <gabrielcosta08@gmail.com>
2023-08-20 10:44:31 +0100

    Update activity to be a entry point for hilt

--- app/src/main/java/pl/inpost/recruitmenttask/InPostActivity.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/InPostActivity.kt
@@ -3,8 +3,9 @@ package pl.inpost.recruitmenttask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InPostActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
