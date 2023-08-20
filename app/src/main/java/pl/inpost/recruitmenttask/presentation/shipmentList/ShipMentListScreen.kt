825d41c8ed1016191475a4cba7aa4dd072455ee4
Gabriel Goto <gabrielcosta08@gmail.com>
2023-08-20 10:44:31 +0100

    Update Shipment Screen to use Scaffold
    - Add top bar

--- app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipMentListScreen.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/ShipMentListScreen.kt
@@ -1,46 +1,79 @@
 package pl.inpost.recruitmenttask.presentation.shipmentList
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.motion.utils.ViewState
 import androidx.hilt.navigation.compose.hiltViewModel
+import pl.inpost.recruitmenttask.R
 import pl.inpost.recruitmenttask.presentation.shipmentList.presenter.ShipmentListViewModel
 import pl.inpost.recruitmenttask.presentation.shipmentList.presenter.model.ShipmentItemType
+import pl.inpost.recruitmenttask.presentation.shipmentList.presenter.model.ShipmentUiModel
 import pl.inpost.recruitmenttask.presentation.shipmentList.ui.ItemSeparator
 import pl.inpost.recruitmenttask.presentation.shipmentList.ui.ShipmentItemComposable
+import pl.inpost.recruitmenttask.presentation.shipmentList.ui.ShipmentItemUIModel
+import pl.inpost.recruitmenttask.theme.InPostTheme
 
 @Composable
 fun ShipmentListScreen(
     viewModel: ShipmentListViewModel = hiltViewModel()
 ) {
-    val state by remember { viewModel.viewState }
+    Scaffold(
+        backgroundColor = Color(0xFFF2F2F2),
+        topBar = {
+            ShipmentTopBar()
+        },
+        content = { paddingValues ->
+            ShipListScreenContent(viewModel.viewState.value)
+        }
+    )
+}
 
-    Surface(
-        color = Color(0xFFF2F2F2)
+@Composable
+private fun ShipmentTopBar() {
+    TopAppBar(
+        contentPadding = PaddingValues(start = 20.dp),
+        backgroundColor = MaterialTheme.colors.background
     ) {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
+        Text(
+            text = stringResource(id = R.string.app_name),
+            color = Color(0xFF404041),
+            style = InPostTheme.typography.bodyLarge,
+        )
+    }
+}
 
-            items(state.items) {
-                when(it){
-                    is ShipmentItemType.HeaderItem -> {
-                        ItemSeparator(text = it.name)
-                    }
-                    is ShipmentItemType.ShipmentItem -> {
-                        ShipmentItemComposable(
-                            modifier = Modifier.fillMaxWidth(),
-                            model = it.data,
-                        )
-                    }
+@Composable
+private fun ShipListScreenContent(state: ShipmentUiModel) {
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+
+        items(state.items) {
+            when(it){
+                is ShipmentItemType.HeaderItem -> {
+                    ItemSeparator(text = it.name)
+                }
+                is ShipmentItemType.ShipmentItem -> {
+                    ShipmentItemComposable(
+                        modifier = Modifier.fillMaxWidth(),
+                        model = it.data,
+                    )
                 }
             }
         }
