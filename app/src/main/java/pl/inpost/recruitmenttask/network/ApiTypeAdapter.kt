241a02ef4453fc0398db581e90daa82a23d58431
Gabriel Goto <gabrielcosta08@gmail.com>
2023-08-20 10:44:31 +0100

    Update TypeAdapter to support room

--- app/src/main/java/pl/inpost/recruitmenttask/network/ApiTypeAdapter.kt
+++ app/src/main/java/pl/inpost/recruitmenttask/network/ApiTypeAdapter.kt
@@ -1,5 +1,6 @@
 package pl.inpost.recruitmenttask.network
 
+import androidx.room.TypeConverter
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import dagger.Reusable
@@ -12,9 +13,11 @@ class ApiTypeAdapter @Inject constructor() {
 
     private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
+    @TypeConverter
     @FromJson
     fun toZonedDateTime(value: String): ZonedDateTime = formatter.parse(value, ZonedDateTime::from)
 
+    @TypeConverter
     @ToJson
     fun fromZonedDateTime(date: ZonedDateTime?): String? = date?.format(formatter)
 }
