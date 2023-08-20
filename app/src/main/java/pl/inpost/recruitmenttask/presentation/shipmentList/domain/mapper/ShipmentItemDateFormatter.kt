50f36fa59d6fce05d252344bb3816f7a51d54879
Gabriel Goto <gabrielcosta08@gmail.com>
2023-08-20 10:44:31 +0100

    Create formatter for dates in shipment

---
+++ app/src/main/java/pl/inpost/recruitmenttask/presentation/shipmentList/domain/mapper/ShipmentItemDateFormatter.kt
@@ -0,0 +1,19 @@
+package pl.inpost.recruitmenttask.presentation.shipmentList.domain.mapper
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
+import javax.inject.Inject
+
+private const val SHIPMENT_DATE_PATTERN = "dd.MM.yy | HH:mm"
+internal class ShipmentItemDateFormatter @Inject constructor() {
+
+    private val formatter = DateTimeFormatter.ofPattern(SHIPMENT_DATE_PATTERN)
+
+    fun format(date: ZonedDateTime): String {
+        val dayOfWeekShort = date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+
+        return "${dayOfWeekShort}. | ${date.format(formatter)}"
+    }
+}
